761a1b45fab68993961f580cc005b6c33b56e082
Ravindu1229 <Rmihiranga2@gmail.com>
2022-05-08 16:59:03 +0530

    Student Insert Alert box update

--- src/com/servlets/StudentInsertServlet.java
+++ src/com/servlets/StudentInsertServlet.java
@@ -13,12 +13,14 @@ import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet("/StudentInsert")
-public class StudentInsert extends HttpServlet {
+public class StudentInsertServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
 		
 		String name = request.getParameter("name");
 		String sid = request.getParameter("sid");
@@ -36,8 +38,15 @@ public class StudentInsert extends HttpServlet {
 		
 		if(isTrue == true)
 		{
-			RequestDispatcher dis = request.getRequestDispatcher("SearchStudent.jsp");
-			dis.forward(request, response);
+			/*
+			 * RequestDispatcher dis = request.getRequestDispatcher("SearchStudent.jsp");
+			 * dis.forward(request, response);
+			 */
+			out.println("<script type='text/javascript'>");
+			out.println("alert('New Student succesfully added!');");
+			out.println("location='SearchStudent.jsp'");
+			out.println("</script>");
+			
 		}
 		else {
 			RequestDispatcher dis2 = request.getRequestDispatcher("unsuccess.jsp");
